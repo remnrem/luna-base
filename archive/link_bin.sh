5f66a88924a328903662c9cf00d52eab000eb363
Senthil Palanivelu <senthilcaesar@yahoo.co.in>
2023-04-15 11:15:27 -0400

    Update link_bin.sh

--- archive/link_bin.sh
+++ archive/link_bin.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Notes ( http://clarkkromenaker.com/post/library-dynamic-loading-mac/ )
+
 myArray=("luna" "destrat" "behead" "fixrows")
 
 mkdir ../mac-arm-luna
@@ -13,11 +15,19 @@ cp ../behead .
 cp ../fixrows .
 
 for str in ${myArray[@]}; do
+
+    # Instructs the loader to search a list of paths to find the dynamic library
+    # Please check the field LC_RPATH in "otool -l luna" output
     install_name_tool -add_rpath @executable_path/. ./$str
+
+    # Change dynamic library loader path
+    # Please check the field LC_LOAD_DYLIB in "otool -l luna" output
     install_name_tool -change /opt/homebrew/opt/fftw/lib/libfftw3.3.dylib @rpath/libfftw3.3.dylib ./$str
     install_name_tool -change /opt/homebrew/opt/libomp/lib/libomp.dylib @rpath/libomp.dylib ./$str
+
 done
 
+# Prints paths to all dynamic libraries used by the executable
 otool -L ./luna
 otool -L ./destrat
 
